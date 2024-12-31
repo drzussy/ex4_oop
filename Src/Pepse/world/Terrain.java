fbe205869d70886f8730577766b32ca558fca13f
drzussy <noamsusman@gmail.com>
2024-12-31 17:55:43 +0200

    complex version of terrain using perlin noise function from util package

--- Src/Pepse/world/Terrain.java
+++ Src/Pepse/world/Terrain.java
@@ -1,6 +1,7 @@
 package Src.Pepse.world;
 
 import Src.Pepse.util.ColorSupplier;
+import Src.Pepse.util.NoiseGenerator;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
 
@@ -15,18 +16,24 @@ public class Terrain {
     private static final Color BASE_GROUND_COLOR = new Color(212, 123, 74);
     private static final String GROUND_BLOCK_TAG = "ground block";
     private static final float DEFAULT_PART_OF_SCREEN_TO_FILL = ((float)2)/3;
-    private final float groundHeightAtX0;
+    public static final int NOISE_FACTOR = 7;
+    private final int groundHeightAtX0;
     private final Vector2 windowDimensions;
+    private final NoiseGenerator noiseGenerator;
 
     //    private int groundHeightAtX0;
     public Terrain(Vector2 windowDimensions, int seed) {
         this.windowDimensions = windowDimensions;
-        groundHeightAtX0 = (DEFAULT_PART_OF_SCREEN_TO_FILL * windowDimensions.y());
+        this.groundHeightAtX0 = (int) (DEFAULT_PART_OF_SCREEN_TO_FILL * windowDimensions.y());
+        this.noiseGenerator = new NoiseGenerator(seed, this.groundHeightAtX0);
     }
 
     //basic method to define ground height for a given x
-    public float groundHeightAt(float x){return groundHeightAtX0;}
+//    public float groundHeightAt(float x){return groundHeightAtX0;}
 
+    public float groundHeightAt(float x) {
+        float noise = (float) noiseGenerator.noise(x, Block.SIZE * NOISE_FACTOR);
+        return groundHeightAtX0 + noise; }
     public List<Block> createInRange(int minX, int maxX){
         ArrayList<Block> blockList = new ArrayList<>();
         for (int i = minX; i < maxX; i=i+Block.SIZE) {
