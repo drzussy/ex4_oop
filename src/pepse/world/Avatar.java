e6078a8cc37bd5b752cac7128e33e195966a2e4e
flabberghast <eyal.hecht2@mail.huji.ac.il>
2025-01-05 19:24:31 +0200

    Fixed bug making the avatar unable to run when on the ground

--- src/pepse/world/Avatar.java
+++ src/pepse/world/Avatar.java
@@ -31,7 +31,7 @@ public class Avatar extends GameObject {
     private static final float VELOCITY_X = 200;
     private static final float VELOCITY_Y = -450;
     private static final float GRAVITY = 700;
-    public static final int AVATAR_SIZE = 40;
+    private static final Vector2 AVATAR_SIZE = new Vector2 (40, 62);
     public static final String BLOCK_TAG = "block";
     public static final double HORIZONTAL_MOVE_ENERGY_DECREASE = -0.5;
     public static final int JUMP_ENERGY_DECREASE = -10;
@@ -41,7 +41,7 @@ public class Avatar extends GameObject {
     private double energy = 100;
 
     public Avatar(Vector2 topLeftCorner, UserInputListener inputListener, ImageReader imageReader){
-        super(topLeftCorner,  Vector2.ONES.mult(AVATAR_SIZE), null);
+        super(topLeftCorner,  AVATAR_SIZE, null);
         idleAnimation = new AnimationRenderable(IDLE_IMAGES_PATHS, imageReader,
                 true, IDLE_ANIMATION_FRAME_TIME);
         jumpingAnimation = new AnimationRenderable(JUMPING_IMAGES_PATHS, imageReader,
@@ -59,20 +59,18 @@ public class Avatar extends GameObject {
         super.update(deltaTime);
         float xVel = 0;
         //check for horizontal movement
-        if (getVelocity().y() == 0) { // TODO: Maybe add check you're not jumping at the peak of the current jump,
+        if (getVelocity().y() == 0 && inputListener.isKeyPressed(KeyEvent.VK_SPACE)) {
+            // try to expend enough energy to jump
+            // TODO: Maybe add check you're not jumping at the peak of the current jump,
             // TODO where your vertical speed is 0 for a brief moment
-            //check for jump
-            if (inputListener.isKeyPressed(KeyEvent.VK_SPACE)) {
-                if (changeEnergy(JUMP_ENERGY_DECREASE)) {
-                    transform().setVelocityY(VELOCITY_Y);
-                    renderer().setRenderable(jumpingAnimation);
-                    // TODO: once observers were implemented, add the following
-                    /*  for (JumpObserver obs : jumpObservers) {
-                            obs.notifyAboutJump();
-                        }
-                     */
-                }
-                // static avatar, add 1 point of energy
+            if (changeEnergy(JUMP_ENERGY_DECREASE)) {
+                transform().setVelocityY(VELOCITY_Y);
+                renderer().setRenderable(jumpingAnimation);
+                // TODO: once observers were implemented, add the following
+                /*  for (JumpObserver obs : jumpObservers) {
+                        obs.notifyAboutJump();
+                    }
+                 */
             }
         } else if (inputListener.isKeyPressed(KeyEvent.VK_LEFT) && !inputListener.isKeyPressed(KeyEvent.VK_RIGHT)) {
             if (changeEnergy(HORIZONTAL_MOVE_ENERGY_DECREASE)) {
@@ -80,16 +78,14 @@ public class Avatar extends GameObject {
                 renderer().setRenderable(runningAnimation);
                 renderer().setIsFlippedHorizontally(true);
             }
-        } else if (inputListener.isKeyPressed(KeyEvent.VK_RIGHT) && !inputListener.isKeyPressed(KeyEvent.VK_LEFT))
-        {
-            if(changeEnergy(HORIZONTAL_MOVE_ENERGY_DECREASE))
-            {
+        } else if (inputListener.isKeyPressed(KeyEvent.VK_RIGHT) && !inputListener.isKeyPressed(KeyEvent.VK_LEFT)) {
+            if (changeEnergy(HORIZONTAL_MOVE_ENERGY_DECREASE)) {
                 xVel += VELOCITY_X;
                 renderer().setRenderable(runningAnimation);
                 renderer().setIsFlippedHorizontally(false);
             }
-        }
-        if (getVelocity().x() == 0 && getVelocity().y() == 0){
+        } else if (getVelocity().x() == 0 && getVelocity().y() == 0){
+            // static avatar, add 1 point of energy
             changeEnergy(1);
             renderer().setRenderable(idleAnimation);
         }
