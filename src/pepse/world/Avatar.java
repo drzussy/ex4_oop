14f20f167a0c38a865b60f7538ca71468685fb3b
flabberghast <eyal.hecht2@mail.huji.ac.il>
2025-01-05 19:00:42 +0200

    Added animations, jumping while walking and no energy drain if moving in midair. Also wrote future code for notifying clouds about jumps

--- src/pepse/world/Avatar.java
+++ src/pepse/world/Avatar.java
@@ -2,17 +2,32 @@ package src.pepse.world;
 
 import danogl.GameObject;
 import danogl.collisions.Collision;
-import danogl.components.CoordinateSpace;
 import danogl.gui.ImageReader;
 import danogl.gui.UserInputListener;
-import danogl.gui.rendering.Renderable;
+import danogl.gui.rendering.AnimationRenderable;
 import danogl.util.Vector2;
 
 import java.awt.event.KeyEvent;
 
 public class Avatar extends GameObject {
 
-    public static final String IMAGE_PATH = "assets/assets/idle_0.png";
+    private static final String[] IDLE_IMAGES_PATHS = {
+            "assets/idle_0.png", "assets/idle_1.png", "assets/idle_2.png", "assets/idle_3.png"
+    };
+    private static final String[] JUMPING_IMAGES_PATHS = {
+            "assets/jump_0.png", "assets/jump_1.png", "assets/jump_2.png", "assets/jump_3.png"
+    };
+    private static final String[] RUNNING_IMAGES_PATHS = {
+            "assets/run_0.png", "assets/run_1.png", "assets/run_2.png",
+            "assets/run_3.png", "assets/run_4.png", "assets/run_5.png"
+    };
+    private static final double IDLE_ANIMATION_FRAME_TIME = 0.3;
+    private static final double JUMP_ANIMATION_FRAME_TIME = 0.5;
+    private static final double RUN_ANIMATION_FRAME_TIME = 0.1;
+    private final AnimationRenderable idleAnimation;
+    private final AnimationRenderable jumpingAnimation;
+    private final AnimationRenderable runningAnimation;
+    // TODO: Implement jump observers. Something like: private final List<JumpObserver> jumpObservers = new List<>();
     private static final float VELOCITY_X = 200;
     private static final float VELOCITY_Y = -450;
     private static final float GRAVITY = 700;
@@ -26,13 +41,17 @@ public class Avatar extends GameObject {
     private double energy = 100;
 
     public Avatar(Vector2 topLeftCorner, UserInputListener inputListener, ImageReader imageReader){
-        super(topLeftCorner,  Vector2.ONES.mult(AVATAR_SIZE),
-                imageReader.readImage(IMAGE_PATH, true));
+        super(topLeftCorner,  Vector2.ONES.mult(AVATAR_SIZE), null);
+        idleAnimation = new AnimationRenderable(IDLE_IMAGES_PATHS, imageReader,
+                true, IDLE_ANIMATION_FRAME_TIME);
+        jumpingAnimation = new AnimationRenderable(JUMPING_IMAGES_PATHS, imageReader,
+                true, JUMP_ANIMATION_FRAME_TIME);
+        runningAnimation = new AnimationRenderable(RUNNING_IMAGES_PATHS, imageReader,
+                true, RUN_ANIMATION_FRAME_TIME);
+        renderer().setRenderable(idleAnimation);
         physics().preventIntersectionsFromDirection(Vector2.ZERO);
         transform().setAccelerationY(GRAVITY);
         this.inputListener = inputListener;
-
-
     }
 
     @Override
@@ -40,33 +59,41 @@ public class Avatar extends GameObject {
         super.update(deltaTime);
         float xVel = 0;
         //check for horizontal movement
-        if (inputListener.isKeyPressed(KeyEvent.VK_LEFT) && !inputListener.isKeyPressed(KeyEvent.VK_RIGHT)) {
+        if (getVelocity().y() == 0) { // TODO: Maybe add check you're not jumping at the peak of the current jump,
+            // TODO where your vertical speed is 0 for a brief moment
+            //check for jump
+            if (inputListener.isKeyPressed(KeyEvent.VK_SPACE)) {
+                if (changeEnergy(JUMP_ENERGY_DECREASE)) {
+                    transform().setVelocityY(VELOCITY_Y);
+                    renderer().setRenderable(jumpingAnimation);
+                    // TODO: once observers were implemented, add the following
+                    /*  for (JumpObserver obs : jumpObservers) {
+                            obs.notifyAboutJump();
+                        }
+                     */
+                }
+                // static avatar, add 1 point of energy
+            }
+        } else if (inputListener.isKeyPressed(KeyEvent.VK_LEFT) && !inputListener.isKeyPressed(KeyEvent.VK_RIGHT)) {
             if (changeEnergy(HORIZONTAL_MOVE_ENERGY_DECREASE)) {
                 xVel -= VELOCITY_X;
+                renderer().setRenderable(runningAnimation);
+                renderer().setIsFlippedHorizontally(true);
             }
         } else if (inputListener.isKeyPressed(KeyEvent.VK_RIGHT) && !inputListener.isKeyPressed(KeyEvent.VK_LEFT))
         {
             if(changeEnergy(HORIZONTAL_MOVE_ENERGY_DECREASE))
             {
                 xVel += VELOCITY_X;
-            }
-        } else if (getVelocity().y() == 0) {
-            //check for jump
-            if (inputListener.isKeyPressed(KeyEvent.VK_SPACE)) {
-                if (changeEnergy(JUMP_ENERGY_DECREASE)) {
-                    transform().setVelocityY(VELOCITY_Y);
-                }
-                // static avatar, add 1 point of energy
+                renderer().setRenderable(runningAnimation);
+                renderer().setIsFlippedHorizontally(false);
             }
         }
-            if (getVelocity().x() == 0 && getVelocity().y() == 0){
-                changeEnergy(1);
-
+        if (getVelocity().x() == 0 && getVelocity().y() == 0){
+            changeEnergy(1);
+            renderer().setRenderable(idleAnimation);
         }
         transform().setVelocityX(xVel);
-
-//        System.out.println(getEnergy());
-
     }
 
     @Override
@@ -84,14 +111,16 @@ public class Avatar extends GameObject {
     }
 
     public boolean changeEnergy(double change){
-        if(energy+change<0){
-            return false;
-        } else if (energy+change>FULL_ENERGY) {
-            energy = FULL_ENERGY;
-        }
-        else{
-            energy = energy+change;
-        }
+        if(energy+change<0) return false;
+        if (getVelocity().y()==0) energy = Math.min(energy+change, FULL_ENERGY);
         return true;
     }
+
+    // TODO: uncomment this after implementing JumpObserver code
+//     public void addJumpObserver (JumpObserver obs) {
+//        this.jumpObservers.add(obs);
+//     }
+//    public void removeJumpObserver (JumpObserver obs) {
+//        this.jumpObservers.remove(obs);
+//    }
 }
