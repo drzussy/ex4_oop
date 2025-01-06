31cd94bcf86dedcaf3d6fadf44cbc0cd73536cd7
flabberghast <eyal.hecht2@mail.huji.ac.il>
2025-01-06 21:29:13 +0200

    Added the infinite world generation, and cleaned up initializeGame() by using the new generation subroutine

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -17,6 +17,7 @@ import src.pepse.world.trees.*;
 import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class PepseGameManager extends GameManager{
 
@@ -27,8 +28,15 @@ public class PepseGameManager extends GameManager{
     private static final float FRUIT_RESPAWN_DELAY = 5;
     private static final float CLOUD_HEIGHT_FRACTION = 0.1F;
     private static final Vector2 CLOUD_DIMENSIONS = new Vector2(300, 160);
+    private static final int LEAF_LAYER = 50;
+    private static final int CHUNK_SIZE = 1500;
     private GameObjectCollection gameObjects;
+    private Avatar avatar;
+    private Terrain terrain;
+    private Flora flora;
     private Vector2 windowDimensions;
+    private int minLoadedX;
+    private int maxLoadedX;
 
 
     public static void main(String[] args){
@@ -44,7 +52,7 @@ public class PepseGameManager extends GameManager{
      * @param inputListener Contains a single method: isKeyPressed, which returns whether
      *                      a given key is currently pressed by the user or not. See its
      *                      documentation.
-     * @param windowController Contains an array of helpful, self explanatory methods
+     * @param windowController Contains an array of helpful, self-explanatory methods
      *                         concerning the window.
      */
     @Override
@@ -55,46 +63,14 @@ public class PepseGameManager extends GameManager{
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
         windowController.setTargetFramerate(60);
         windowDimensions = windowController.getWindowDimensions();
+        float windowWidth = windowDimensions.x();
+        minLoadedX = (int) (-CHUNK_SIZE);
+        maxLoadedX = (int) ((windowWidth/Block.SIZE)*Block.SIZE+CHUNK_SIZE);
+
         //initialize sky background and set to layer
         GameObject sky = Sky.create(windowDimensions);
         gameObjects = gameObjects();
         gameObjects.addGameObject(sky, Layer.BACKGROUND);
-
-        //terrain initialization
-        Terrain terrain = new Terrain(windowDimensions, new Random().nextInt());
-        List<Block> terrainList = terrain.createInRange(0, (int) windowDimensions.x());
-        for(Block block: terrainList){
-            gameObjects.addGameObject(block, Layer.STATIC_OBJECTS);
-        }
-
-        Consumer<GameObject> eatFruit = (GameObject f) -> {
-            gameObjects.removeGameObject(f, Layer.STATIC_OBJECTS);
-            new ScheduledTask(
-                    f, FRUIT_RESPAWN_DELAY,false,
-                    () -> gameObjects.addGameObject(f, Layer.STATIC_OBJECTS) // Action to re-add the fruit
-            );
-        };
-
-        // cloud creation
-        List<GameObject> cloud = Cloud.create(new Vector2(0, windowDimensions.y()*CLOUD_HEIGHT_FRACTION),
-                CLOUD_DIMENSIONS, windowDimensions);
-        if (cloud!=null) {
-            for (GameObject cloudBlock : cloud) {
-                gameObjects.addGameObject(cloudBlock, Layer.BACKGROUND);
-            }
-        }
-
-        Flora flora = new Flora(new Random().nextInt(), terrain::surfaceLevelAt, imageReader::readImage);
-        Map<Tree, List<GameObject>> treeList = flora.createInRange(0, (int) windowDimensions.x());
-        for(Tree tree: treeList.keySet()){
-            gameObjects.addGameObject(tree, Layer.STATIC_OBJECTS);
-            List<GameObject> treeLeavesAndFruit = treeList.get(tree);
-            for (GameObject obj : treeLeavesAndFruit) {
-                if (obj.getTag().equals("leaf")) gameObjects.addGameObject(obj, Layer.BACKGROUND);
-                else gameObjects.addGameObject(obj, Layer.STATIC_OBJECTS);
-            }
-        }
-
         // night initialization
         GameObject night = Night.create(windowDimensions, DEFAULT_CYCLE_LENGTH);
         gameObjects.addGameObject(night, Layer.FOREGROUND);
@@ -107,22 +83,91 @@ public class PepseGameManager extends GameManager{
         GameObject sunHalo = SunHalo.create(sun);
         gameObjects.addGameObject(sunHalo, Layer.BACKGROUND);
 
+        // cloud creation
+        List<GameObject> cloud = Cloud.create(new Vector2(0, windowDimensions.y()*CLOUD_HEIGHT_FRACTION),
+                CLOUD_DIMENSIONS, windowDimensions);
+        if (cloud!=null) {
+            for (GameObject cloudBlock : cloud) {
+                gameObjects.addGameObject(cloudBlock, Layer.BACKGROUND);
+            }
+        }
+
+        //terrain initialization
+        terrain = new Terrain(windowDimensions, new Random().nextInt());
+        flora = new Flora(new Random().nextInt(), terrain::surfaceLevelAt, imageReader::readImage);
+
+        loadWorld(minLoadedX, maxLoadedX);
+//        List<Block> terrainList = terrain.createInRange(minLoadedX, maxLoadedX);
+//        Map<Tree, List<GameObject>> treeList = flora.createInRange(minLoadedX, maxLoadedX);
+//        for(Block block: terrainList){
+//            gameObjects.addGameObject(block, Layer.STATIC_OBJECTS);
+//        }
+//        for(Tree tree: treeList.keySet()){
+//            gameObjects.addGameObject(tree, Layer.STATIC_OBJECTS);
+//            List<GameObject> treeLeavesAndFruit = treeList.get(tree);
+//            for (GameObject obj : treeLeavesAndFruit) {
+//                if (obj.getTag().equals("leaf")) gameObjects.addGameObject(obj, LEAF_LAYER);
+//                else gameObjects.addGameObject(obj, Layer.STATIC_OBJECTS);
+//            }
+//        }
+        gameObjects.layers().shouldLayersCollide(Layer.FOREGROUND, Layer.DEFAULT, false);
+
         // avatar initialization
         float middle_x = (float) (windowDimensions.x()* MIDDLE);
         Vector2 avatarInitialPosition = new Vector2(middle_x, terrain.groundHeightAt(middle_x)- PLACEMENT_BUFFER);
-        Avatar avatar =
-                new Avatar(avatarInitialPosition,
-                inputListener, imageReader);
+        avatar = new Avatar(avatarInitialPosition, inputListener, imageReader);
         gameObjects.addGameObject(avatar);
         avatar.setTag("avatar"); // TODO: move to someplace else
 
-        ValueProvider callback = avatar::getEnergy;
-        gameObjects.addGameObject(new EnergyDisplay(Vector2.ZERO ,windowDimensions, callback));
+        // Create energy display
+        Supplier<Double> callback = avatar::getEnergy;
+        gameObjects.addGameObject(new EnergyDisplay(Vector2.ZERO ,windowDimensions, callback), Layer.UI);
+
+        // TODO - remove this
+        Supplier<Double> locationCallback = () -> (double) avatar.getCenter().x();
+        gameObjects.addGameObject(new EnergyDisplay(new Vector2(windowWidth*0.5f, 0) ,windowDimensions, locationCallback), Layer.UI);
 
         Vector2 cameraPosition = new Vector2(0, -windowDimensions.y()*CAMERA_HEIGHT);
         setCamera(new Camera(avatar, cameraPosition,
                 windowController.getWindowDimensions(),
                 windowController.getWindowDimensions()));
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        if (avatar.getCenter().x()-minLoadedX < CHUNK_SIZE) {
+            loadWorld(minLoadedX-CHUNK_SIZE, minLoadedX);
+            minLoadedX -= CHUNK_SIZE;
+            maxLoadedX -= CHUNK_SIZE;
+        }
+        else if (maxLoadedX-avatar.getCenter().x() < CHUNK_SIZE) {
+            loadWorld(maxLoadedX, maxLoadedX+CHUNK_SIZE);
+            minLoadedX += CHUNK_SIZE;
+            maxLoadedX += CHUNK_SIZE;
+        }
+        for (GameObject obj : gameObjects) {
+            if (obj.getTopLeftCorner().x()<minLoadedX || obj.getTopLeftCorner().x()>maxLoadedX) {
+                gameObjects.removeGameObject(obj, LEAF_LAYER); // Removes only leaves
+                gameObjects.removeGameObject(obj, Layer.STATIC_OBJECTS); // removes only trees and ground
+            }
+        }
+    }
+
+    private void loadWorld (int minX, int maxX) {
+        List<Block> terrainList = terrain.createInRange(minX, maxX);
+        for(Block block: terrainList){
+            gameObjects.addGameObject(block, Layer.STATIC_OBJECTS);
+        }
+        Map<Tree, List<GameObject>> treeList = flora.createInRange(minX, maxX);
+        for(Tree tree: treeList.keySet()){
+            gameObjects.addGameObject(tree, Layer.STATIC_OBJECTS);
+            List<GameObject> treeLeavesAndFruit = treeList.get(tree);
+            for (GameObject obj : treeLeavesAndFruit) {
+                if (obj.getTag().equals(Flora.LEAF_TAG)) gameObjects.addGameObject(obj, LEAF_LAYER);
+                else gameObjects.addGameObject(obj, Layer.STATIC_OBJECTS);
+            }
+        }
     }
 }
 
