86b4521b8cd4ce631c2aff482b9fbf240dee6c66
flabberghast <eyal.hecht2@mail.huji.ac.il>
2025-01-06 13:24:48 +0200

    Cleaned up code

--- src/pepse/world/trees/Flora.java
+++ src/pepse/world/trees/Flora.java
@@ -1,6 +1,5 @@
 package src.pepse.world.trees;
 
-import danogl.GameObject;
 import danogl.components.ScheduledTask;
 import danogl.components.Transition;
 import danogl.gui.rendering.RectangleRenderable;
@@ -20,8 +19,6 @@ public class Flora {
     private static final int CANOPY_HEIGHT = 5;
     private static final int CANOPY_WIDTH = 7;
     private static final Color LEAF_COLOR = new Color(76, 253, 52);
-    private static final RectangleRenderable LEAF_RENDERABLE = new RectangleRenderable(LEAF_COLOR);
-//    private static final Vector2 LEAF_SIZE = new Vector2(60, 90);
     private static final float LEAF_CHANCE = 0.6F;
     private static final float LEAF_MAX_ANGLE = 45F;
     private static final float LEAF_CYCLE_LENGTH = 2F;
@@ -29,16 +26,12 @@ public class Flora {
     private static final float LEAF_WIND_HEIGHT_FACTOR = 0.95f;
     private static final Color TREE_TRUNK_COLOR = new Color(124, 51, 10);
     private static final int TREE_COLOR_DELTA = 50;
-    private final Vector2 windowDimensions;
-//    private final NoiseGenerator noiseGenerator;
+    private static final int LEAF_COLOR_DELTA = 30;
     private final Random random;
     private final Function<Float, Float> getGroundHeightAt;
-//    private static final int TREE_NOISE_FACTOR = 10;
-    private final static Renderable TREE_TRUNK = new RectangleRenderable(new Color(124, 51, 10));
     private final int seed;
 
-    public Flora(Vector2 windowDimensions, int seed, Function<Float, Float> getGroundHeightAt) {
-        this.windowDimensions = windowDimensions;
+    public Flora(int seed, Function<Float, Float> getGroundHeightAt) {
         this.seed = seed;
         this.random = new Random(seed);
         this.getGroundHeightAt = getGroundHeightAt;
@@ -50,58 +43,65 @@ public class Flora {
     }
 
     public Map<Tree, List<Block>> createInRange(int minX, int maxX){
-//        ArrayList<Tree> treeList = new ArrayList<>();
-//
         Map<Tree, List<Block>> map = new HashMap<>();
         minX = ((minX + Block.SIZE - 1) / Block.SIZE) * Block.SIZE;
         maxX = (maxX / Block.SIZE) * Block.SIZE;
         for (float x = minX; x < maxX; x=x+Block.SIZE) {
             if (random.nextFloat()<0.2f) { // create tree for 20% of blocks
-                float treeHeight = treeHeightAtInBlocks(x) * Block.SIZE;
-                Vector2 treeTopLeftCorner = new Vector2(x, getGroundHeightAt.apply(x) - treeHeight);
-                Vector2 treeSize = new Vector2(Block.SIZE, treeHeight);
-                Renderable treeRender = new RectangleRenderable(
-                        ColorSupplier.approximateColor(TREE_TRUNK_COLOR, TREE_COLOR_DELTA));
-                Tree tree = new Tree (treeTopLeftCorner, treeSize, treeRender);
-                float rightShift = (float) (CANOPY_WIDTH-1)*Block.SIZE/2;
-                float downShift = (float) (CANOPY_HEIGHT*Block.SIZE)/2;
-                Random tempRand = new Random(Objects.hash(x, seed));
-                ArrayList<Block> leafList = new ArrayList<>();
-                for (int row=0; row<CANOPY_HEIGHT; row++) {
-                    for (int col=0; col<CANOPY_WIDTH; col++) {
-                        if (tempRand.nextFloat()<LEAF_CHANCE) {
-                            Renderable leafRender = new RectangleRenderable(
-                                    ColorSupplier.approximateColor(LEAF_COLOR));
-                            Vector2 leafTopLeft = treeTopLeftCorner.add(new Vector2(
-                                    col*Block.SIZE-rightShift, row*Block.SIZE-downShift));
-                            Block leaf = new Block(leafTopLeft, leafRender);
-                            Runnable createLeafTransitions = () -> {
-                                new Transition<>(leaf,
-                                        leaf.renderer()::setRenderableAngle,
-                                        0f,
-                                        LEAF_MAX_ANGLE,
-                                        Transition.LINEAR_INTERPOLATOR_FLOAT,
-                                        LEAF_CYCLE_LENGTH,
-                                        Transition.TransitionType.TRANSITION_BACK_AND_FORTH,
-                                        null);
-                                new Transition<>(leaf,
-                                        leaf::setDimensions,
-                                        new Vector2(Block.SIZE, Block.SIZE),
-                                        new Vector2(Block.SIZE*LEAF_WIND_WIDTH_FACTOR, Block.SIZE*LEAF_WIND_HEIGHT_FACTOR),
-                                        Transition.LINEAR_INTERPOLATOR_VECTOR,
-                                        LEAF_CYCLE_LENGTH,
-                                        Transition.TransitionType.TRANSITION_BACK_AND_FORTH,
-                                        null);
-                            };
-                            float delayTime = tempRand.nextFloat()*2;
-                            new ScheduledTask(leaf, delayTime, false, createLeafTransitions);
-                            leafList.add(leaf);
-                        }
-                    }
-                }
+                Tree tree = generateTree(x);
+                ArrayList<Block> leafList = generateLeaves(x, tree.getTopLeftCorner());
                 map.put(tree, leafList);
             }
         }
         return map;
+    }
+
+    private Tree generateTree (float x) {
+        float treeHeight = treeHeightAtInBlocks(x) * Block.SIZE;
+        Vector2 treeTopLeftCorner = new Vector2(x, getGroundHeightAt.apply(x) - treeHeight);
+        Vector2 treeSize = new Vector2(Block.SIZE, treeHeight);
+        Renderable treeRender = new RectangleRenderable(
+                ColorSupplier.approximateColor(TREE_TRUNK_COLOR, TREE_COLOR_DELTA));
+        return new Tree (treeTopLeftCorner, treeSize, treeRender);
+    }
+
+    private ArrayList<Block> generateLeaves(float x, Vector2 treeTopLeftCorner) {
+        ArrayList<Block> leafList = new ArrayList<>();
+        Random tempRand = new Random(Objects.hash(x, seed));
+        float rightShift = (float) (CANOPY_WIDTH-1)*Block.SIZE/2;
+        float downShift = (float) (CANOPY_HEIGHT*Block.SIZE)/2;
+        for (int row=0; row<CANOPY_HEIGHT; row++) {
+            for (int col=0; col<CANOPY_WIDTH; col++) {
+                if (tempRand.nextFloat()<LEAF_CHANCE) {
+                    Renderable leafRender = new RectangleRenderable(
+                            ColorSupplier.approximateColor(LEAF_COLOR, LEAF_COLOR_DELTA));
+                    Vector2 leafTopLeft = treeTopLeftCorner.add(new Vector2(
+                            col*Block.SIZE- rightShift, row*Block.SIZE- downShift));
+                    Block leaf = new Block(leafTopLeft, leafRender);
+                    Runnable createLeafTransitions = () -> {
+                        new Transition<>(leaf,
+                                leaf.renderer()::setRenderableAngle,
+                                0f,
+                                LEAF_MAX_ANGLE,
+                                Transition.LINEAR_INTERPOLATOR_FLOAT,
+                                LEAF_CYCLE_LENGTH,
+                                Transition.TransitionType.TRANSITION_BACK_AND_FORTH,
+                                null);
+                        new Transition<>(leaf,
+                                leaf::setDimensions,
+                                new Vector2(Block.SIZE, Block.SIZE),
+                                new Vector2(Block.SIZE*LEAF_WIND_WIDTH_FACTOR, Block.SIZE*LEAF_WIND_HEIGHT_FACTOR),
+                                Transition.LINEAR_INTERPOLATOR_VECTOR,
+                                LEAF_CYCLE_LENGTH,
+                                Transition.TransitionType.TRANSITION_BACK_AND_FORTH,
+                                null);
+                    };
+                    float delayTime = tempRand.nextFloat()*2;
+                    new ScheduledTask(leaf, delayTime, false, createLeafTransitions);
+                    leafList.add(leaf);
+                }
+            }
+        }
+        return leafList;
     }
 }
