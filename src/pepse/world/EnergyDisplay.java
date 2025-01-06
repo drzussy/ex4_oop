07ac0b6b955476421a4b24379dff2ea1df15768f
flabberghast <eyal.hecht2@mail.huji.ac.il>
2025-01-06 13:27:27 +0200

    Cleaned up code, changed to integer instead of Double, changed name to EnergyDisplay (instead of HealthDisplay)

---
+++ src/pepse/world/EnergyDisplay.java
@@ -0,0 +1,35 @@
+package src.pepse.world;
+
+import danogl.GameObject;
+import danogl.components.CoordinateSpace;
+import danogl.gui.rendering.TextRenderable;
+import danogl.util.Vector2;
+
+public class EnergyDisplay extends GameObject {
+
+    public static final String FULL_HEALTH = "100";
+    private final ValueProvider callback;
+
+    /**
+     * Construct a new HealthDisplay instance.
+     *
+     * @param topLeftCorner Position of the display, in window coordinates (pixels).
+     *                      Note: this will stay constant with respect to the camera.
+     * @param dimensions    Width and height in window coordinates.
+     */
+    public EnergyDisplay(Vector2 topLeftCorner, Vector2 dimensions, ValueProvider callback
+                         ) {
+        super(topLeftCorner, Vector2.ONES.mult(100), new TextRenderable(FULL_HEALTH));
+        this.callback = callback;
+        setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        this.renderer().setRenderable(new TextRenderable(
+                Integer.toString((int) Math.floor(callback.getValue()))));
+
+    }
+}
+
