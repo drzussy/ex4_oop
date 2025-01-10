6fe4124fa4c295b1e4efc3e7863ae5726a40fa0f
flabberghast <eyal.hecht2@mail.huji.ac.il>
2025-01-10 15:38:25 +0200

    Extended API to add support for different-color and different-initial-angle suns and halos

--- src/pepse/world/daynight/Sun.java
+++ src/pepse/world/daynight/Sun.java
@@ -5,9 +5,7 @@ import danogl.components.CoordinateSpace;
 import danogl.components.Transition;
 import danogl.gui.rendering.OvalRenderable;
 import danogl.util.Vector2;
-
 import java.awt.*;
-
 import static src.pepse.util.PepseConstants.SUN_TAG;
 
 /**
@@ -18,30 +16,62 @@ public class Sun {
     private static final float FULL_ROTATION_DEGREES = 360f;
     private static final float DIMENSIONS_CENTER = 0.5f;
     private static final int SUN_SIZE = 50;
-    private static final float HEIGHT_AT_ZENITH = 1f / 3;
+    private static final float HEIGHT_AT_ZENITH = 1f / 6;
 
-    /** A static method for creating a sun GameObject, which will orbit the center of the screen
-     * (moving with the character).
+    /** A static method for creating a default sun GameObject, which will orbit the center of the screen
+     * counterclockwise (and moving with the character). The default sun is yellow and starts at zenith.
      * @param windowDimensions The dimensions of the game window, around which the sun revolves.
      * @param cycleLength The length of a day/night cycle.
      * @return The sun GameObject, which already handles day/night rotation.
      */
     public static GameObject create(Vector2 windowDimensions, float cycleLength){
+        // TODO: delete the following code
+//        Vector2 initialSunCenter = new Vector2(windowDimensions.x()*DIMENSIONS_CENTER,
+//                windowDimensions.y()* HEIGHT_AT_ZENITH);
+//        GameObject sun = new GameObject(initialSunCenter, new Vector2(SUN_SIZE, SUN_SIZE),
+//                new OvalRenderable(Color.YELLOW));
+//        sun.setCenter(initialSunCenter);
+//        sun.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+//        sun.setTag(SUN_TAG);
+//
+//        Vector2 cycleCenter = new Vector2(windowDimensions.x()* DIMENSIONS_CENTER,
+//                HORIZON_RATIO * windowDimensions.y());
+//        new Transition<>(sun,
+//                (Float angle)->
+//                        sun.setCenter(initialSunCenter.subtract(cycleCenter).rotated(angle).add(cycleCenter)),
+//                0f,
+//                FULL_ROTATION_DEGREES,
+//                Transition.LINEAR_INTERPOLATOR_FLOAT,
+//                cycleLength,
+//                Transition.TransitionType.TRANSITION_LOOP,
+//                null);
+        return create(windowDimensions, cycleLength, Color.YELLOW, 0);
+    }
+
+    /** A static method for creating a more detailed sun GameObject, which will orbit the center of the screen
+     * (moving with the character). Allows specifying the desired color and initial angle (counterclockwise).
+     * @param windowDimensions The dimensions of the game window, around which the sun revolves.
+     * @param cycleLength The length of a day/night cycle.
+     * @param color The color of the sun to be created.
+     * @param initialAngle The initial
+     * @return The sun GameObject, which already handles day/night rotation.
+     */
+    public static GameObject create(Vector2 windowDimensions, float cycleLength,
+                                    Color color, float initialAngle){
         Vector2 initialSunCenter = new Vector2(windowDimensions.x()*DIMENSIONS_CENTER,
                 windowDimensions.y()* HEIGHT_AT_ZENITH);
         GameObject sun = new GameObject(initialSunCenter, new Vector2(SUN_SIZE, SUN_SIZE),
-                new OvalRenderable(Color.YELLOW));
+                new OvalRenderable(color));
         sun.setCenter(initialSunCenter);
         sun.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
         sun.setTag(SUN_TAG);
-
         Vector2 cycleCenter = new Vector2(windowDimensions.x()* DIMENSIONS_CENTER,
                 HORIZON_RATIO * windowDimensions.y());
         new Transition<>(sun,
                 (Float angle)->
                         sun.setCenter(initialSunCenter.subtract(cycleCenter).rotated(angle).add(cycleCenter)),
-                0f,
-                FULL_ROTATION_DEGREES,
+                initialAngle,
+                initialAngle+FULL_ROTATION_DEGREES,
                 Transition.LINEAR_INTERPOLATOR_FLOAT,
                 cycleLength,
                 Transition.TransitionType.TRANSITION_LOOP,
--- src/pepse/world/daynight/SunHalo.java
+++ src/pepse/world/daynight/SunHalo.java
@@ -11,17 +11,36 @@ import static src.pepse.util.PepseConstants.SUN_HALO_TAG;
  * A class to create halos for already-created suns. Used via its static method, create(sun).
  */
 public class SunHalo {
+    private static final int HALO_OPACITY = 60;
     private static final Color HALO_COLOR = new Color(255, 255, 0, 60);
     private static final Vector2 HALO_BUFFER = new Vector2(20, 20);
 
     /**
-     * A static method to create a halo for the sun.
+     * A static method to create a default halo for the sun.
      * @param sun The sun to have a halo created for.
      * @return The halo for the passed sun.
      */
     public static GameObject create(GameObject sun){
+        // TODO: delete the following code
+//        GameObject sunHalo = new GameObject(sun.getTopLeftCorner(), sun.getDimensions().add(HALO_BUFFER),
+//                new OvalRenderable(HALO_COLOR));
+//        sunHalo.addComponent((float deltaTime)->sunHalo.setCenter(sun.getCenter()));
+//        sunHalo.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
+//        sunHalo.setTag(SUN_HALO_TAG);
+//        return sunHalo;
+        return create(sun, HALO_COLOR);
+    }
+    /**
+     * A static method to create a custom-colored halo for the sun.
+     * @param sun The sun to have a halo created for.
+     * @param color The color of the desired halo. Note: only the RGB values of the color matter,
+     *              as the passed opacity will be overridden.
+     * @return The halo for the passed sun.
+     */
+    public static GameObject create(GameObject sun, Color color){
+        Color haloColor = new Color (color.getRed(), color.getGreen(), color.getBlue(), HALO_OPACITY);
         GameObject sunHalo = new GameObject(sun.getTopLeftCorner(), sun.getDimensions().add(HALO_BUFFER),
-                new OvalRenderable(HALO_COLOR));
+                new OvalRenderable(haloColor));
         sunHalo.addComponent((float deltaTime)->sunHalo.setCenter(sun.getCenter()));
         sunHalo.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
         sunHalo.setTag(SUN_HALO_TAG);
